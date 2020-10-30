04640c3f0abe761b4fa91c8f0053a25576642af8
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 14:32:34 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def uniform_discrete(a,b) :
+  # Your code to generate the uniform discrete random variable goes here
+  
+  
+def n_uniform_discrete( n, a, b ) : 
+  # You code to generate the sum of n uniform discrete random variables goes here
+
+
+# This outputs what you would get when you roll two fair dice four times.
+print( n_uniform_discrete(2,1,6), n_uniform_discrete(2,1,6), n_uniform_discrete(2,1,6), n_uniform_discrete(2,1,6) )
